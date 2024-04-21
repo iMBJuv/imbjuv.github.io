8d5e7d4ddd3262c04133c48c81c1c35b22228a65
iMBJuv <138353126+iMBJuv@users.noreply.github.com>
2024-04-21 17:51:44 +0300

    Delete Template4ECommerce directory

--- Template4ECommerce/js/main.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 100) {
-    headerElement.classList.add("scrolling");
-  } else {
-    headerElement.classList.remove("scrolling");
-  }
-});
